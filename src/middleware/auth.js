86ab40216106f2170f0ed197b278816031061067
Nathan King <nathanking3590@gmail.com>
2022-04-08 11:39:25 +0100

    add auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,53 @@
+import { sendDataResponse } from '../utils/responses.js'
+import { JWT_SECRET } from '../utils/config.js'
+import jwt from 'jsonwebtoken'
+
+export function validateAuthentication(req, res, next) {
+  const header = req.header('authorization')
+
+  if (!header) {
+    return sendDataResponse(res, 401, {
+      authorization: 'Missing Authorization header'
+    })
+  }
+
+  const [type, token] = header.split(' ')
+
+  const isTypeValid = validateTokenType(type)
+  if (!isTypeValid) {
+    return sendDataResponse(res, 401, {
+      authentication: `Invalid token type, expected Bearer but got ${type}`
+    })
+  }
+
+  const isTokenValid = validateToken(token)
+  if (!isTokenValid) {
+    return sendDataResponse(res, 401, {
+      authentication: 'Invalid or missing access token'
+    })
+  }
+
+  next()
+}
+
+function validateToken(token) {
+  if (!token) {
+    return false
+  }
+
+  return jwt.verify(token, JWT_SECRET, (error) => {
+    return !error
+  })
+}
+
+function validateTokenType(type) {
+  if (!type) {
+    return false
+  }
+
+  if (type.toUpperCase() !== 'BEARER') {
+    return false
+  }
+
+  return true
+}
