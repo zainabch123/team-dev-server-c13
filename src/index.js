5419c6b1db02bcd6587a03aedf65c3e31e7bb620
JDC-horizons <jamesdcanterbury@protonmail.com>
2024-09-06 11:36:00 +0100

    minor change

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import app from './server.js'
 const port = process.env.PORT || 4000
 const hostname = process.env.HOSTNAME || 'localhost'
 
-// app.listen(port, hostname, () => {
-//   console.log(`\n Server is running on ${hostname} port ${port}\n`)
-// })
-
-app.listen(port, 'localhost', () => {
-  console.log(`\n Server is running on localhost port ${port}\n`)
+app.listen(port, hostname, () => {
+  console.log(`\n Server is running on ${hostname} port ${port}\n`)
 })
